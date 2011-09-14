387840255387952a0fb65f95b2bce6e1d7705934
bgrins <briangrinstead@gmail.com>
2011-09-14 15:50:07 -0500

    crunching

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-window.log = function(){
-  if(this.console) {
-    arguments.callee = arguments.callee.caller;
-    var newarr = [].slice.call(arguments);
-    (typeof console.log === 'object' ? log.apply.call(console.log, console, newarr) : console.log.apply(console, newarr));
-  }
-};
 var hasStorage = !!(localStorage && JSON);
 var defaultPallet = '{ "#3126c1": { }, "#c8901e": { }, "#c81e59": { }, "#98c39b": { } }';
 
