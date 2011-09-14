1d45764f08f230aa57a01cafdfcd0b74b1de3111
bgrins <briangrinstead@gmail.com>
2011-09-14 15:21:24 -0500

    updating pallets

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ window.log = function(){
   }
 };
 var hasStorage = !!(localStorage && JSON);
+var defaultPallet = '{ "#3126c1": { }, "#c8901e": { }, "#c81e59": { }, "#98c39b": { } }';
 
 function getPallet() {
-    if (!hasStorage) { return { }; }
-    return JSON.parse(localStorage["colors"] || "{ }");
+    if (!hasStorage) { "" }
+    return JSON.parse(localStorage["colors"] || defaultPallet);
 }
 function setPallet(c) {
     if (!hasStorage) { return; }
@@ -59,13 +60,17 @@ $(function() {
     tb.bind("keyup change", function() { setCurrentHex($(this).val()); });
     $("input[readonly]").click(function() { $(this).focus(); this.select(); });
     
-    var hsl = $("#hsl input");
-    var hex = $("#hex input");
-    var rgb = $("#rgb input");
-    var hsv = $("#hsv input");
+    var hsl = $("#hsl input"),
+        hex = $("#hex input"),
+        rgb = $("#rgb input"),
+        hsv = $("#hsv input"),
+        triad = $("#triad"), 
+        tetrad = $("#tetrad"), 
+        mono = $("#monochromatic");
     
     function change(color) {
         var hexVal = color.toHexString();
+        console.log("change", hexVal);
         $("#preview").css("background-color", hexVal);
         tb.css("border-color", hexVal);
         hsv.val(color.toHsvString());
@@ -74,7 +79,9 @@ $(function() {
         hsl.val(color.toHslString());
         url.val(u + hexVal);
         
-        $("#preview").toggleClass("has", palletHas(hexVal));
+        var h = color.toHsv();
+        var isContrast = h.s < .3 && h.v > .6;
+        $("#preview").toggleClass("has", palletHas(hexVal)).toggleClass("contrast", isContrast);
     }
     
     function getCurrentHex() {
@@ -89,18 +96,29 @@ $(function() {
         tb.val((color || s.spectrum("get")).toHexString());
     }
     
+    function updatePartial(color) {
+        var tiny = color || s.spectrum("get");
+	   $("#preview").removeClass("fromScheme");
+	   $(".schemer li").removeClass("active");
+        updateTextbox(tiny);
+        updateSchemes(tiny);
+    }
+    
 	s.spectrum({
 	    color: getLastColor(),
 		flat: true,
 		showInput: false,
 		change: change,
-		move: updateTextbox,
-		show: updateTextbox
+		move: updatePartial,
+		show: updatePartial
 	});
 	
 	$("#preview a").click(function() {
 	   var hex = getCurrentHex();
-	   $(this).is(".add") ? palletAdd(hex) : palletRemove(hex);
+	   $(this).is(".add") && palletAdd(hex);
+	   $(this).is(".remove") && palletRemove(hex);
+	   $(this).is(".reload") && updatePartial();
+    
        $("#preview").toggleClass("has", palletHas(hex));
 	   return false;
 	});
@@ -114,5 +132,44 @@ $(function() {
     
     window.onunload = function() {
         setLastColor(getCurrentHex()); 
+    }
+    
+    
+	
+	
+	// Only keep the scheme color if they click on it
+	// Wait to update schemes until they press add
+	var stored;
+	$(".schemer").hover(
+	   function() { stored = getCurrentHex(); }, 
+	   function() { setCurrentHex(stored, true); }
+    );
+	
+    $(".schemer").delegate("li", "hover", function() {
+	   if ($(".schemer li.active").length == 0) {
+	       setCurrentHex($(this).attr("title"), true);
+	   }
+	   
+	}).delegate("li", "click", function() {
+	   setCurrentHex($(this).attr("title"), true);
+	   $(".schemer li").removeClass("active");
+	   $(this).addClass("active");
+	   stored = getCurrentHex();
+	   $("#preview").addClass("fromScheme");
+	});
+	
+    function schemeTmpl(e) {
+            var hex = e.toHexString();
+            return '<li style="background:'+hex+'" title="'+hex+'" />'
+    }
+    function updateSchemes(tiny) {
+        
+        var combines = $(".schemer").toggleClass("invisible", !tiny.ok);
+        
+        triad.html($.map(tinycolor.triad(tiny), schemeTmpl).join(''));
+        
+        tetrad.html($.map(tinycolor.tetrad(tiny), schemeTmpl).join(''));
+        
+        mono.html($.map(tinycolor.monochromatic(tiny), schemeTmpl).join(''));
     }
 });
